1dd1bc08fd24d6b157c9dfa4c385cc8c9b07bde0
cavanosa <cavanosa@cavanosa.cav>
2020-10-14 12:32:47 +0200

    refresh end

--- src/app/interceptors/prod-interceptor.service.ts
+++ src/app/interceptors/prod-interceptor.service.ts
@@ -1,23 +1,53 @@
+import { AuthService } from './../service/auth.service';
+import { JwtDTO } from './../models/jwt-dto';
+import { catchError, concatMap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { TokenService } from '../service/token.service';
+
+const AUTHORIZATION = 'Authorization';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdInterceptorService implements HttpInterceptor {
 
-  constructor(private tokenService: TokenService) { }
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (!this.tokenService.isLogged()) {
+      return next.handle(req);
+    }
+
     let intReq = req;
     const token = this.tokenService.getToken();
-    if (token != null) {
-      intReq = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + token)});
-    }
-    return next.handle(intReq);
+
+    intReq = this.addToken(req, token);
+
+    return next.handle(intReq).pipe(catchError((err: HttpErrorResponse) => {
+      if (err.status === 401) {
+        const dto: JwtDTO = new JwtDTO(this.tokenService.getToken());
+        return this.authService.refresh(dto).pipe(concatMap((data: any) => {
+          console.log('refreshing....');
+          this.tokenService.setToken(data.token);
+          intReq = this.addToken(req, data.token);
+          return next.handle(intReq);
+        }));
+      } else {
+        this.tokenService.logOut();
+        return throwError(err);
+      }
+    }));
+  }
+
+  private addToken(req: HttpRequest<any>, token: string): HttpRequest<any> {
+    return req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + token) });
   }
 }
 
-export const interceptorProvider = [{provide: HTTP_INTERCEPTORS, useClass: ProdInterceptorService, multi: true}];
+export const interceptorProvider = [{ provide: HTTP_INTERCEPTORS, useClass: ProdInterceptorService, multi: true }];
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -22,4 +22,8 @@ export class AuthService {
   public login(loginUsuario: LoginUsuario): Observable<JwtDTO> {
     return this.httpClient.post<JwtDTO>(this.authURL + 'login', loginUsuario);
   }
+
+  public refresh(dto: JwtDTO): Observable<JwtDTO> {
+    return this.httpClient.post<JwtDTO>(this.authURL + 'refresh', dto);
+  }
 }
